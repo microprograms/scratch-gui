a96117b864888a0a13fa4b85ec77402f5a12bfc9
xuzewei <microprograms@126.com>
2019-11-04 20:06:12 +0800

    国际化

--- src/containers/flyingbears-redo.jsx
+++ src/containers/flyingbears-redo.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import styles from '../components/menu-bar/menu-bar.css';
 import redoIcon from '../components/menu-bar/redo.svg';
 import classNames from 'classnames';
+import {FormattedMessage} from 'react-intl';
 
 class FlyingbearsRedo extends React.Component {
     constructor (props) {
@@ -17,7 +18,12 @@ class FlyingbearsRedo extends React.Component {
         return (
             <div onClick={this.handleChange} className={classNames(styles.menuBarItem, styles.hoverable)} >
                 <img className={styles.icon} src={redoIcon} />
-                <span className={styles.desc}>重做</span>
+                <span className={styles.desc}>
+                    <FormattedMessage
+                        defaultMessage="Redo"
+                        id="gui.menuBar.redo"
+                    />
+                </span>
             </div>
         );
     }
